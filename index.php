5f74dfe787f2bf20a484573575e810d7f4c1bf28
AnnGizmo <omggizmo19@mail.ru>
2024-12-11 17:10:05 +0300

    Итерация по массивам (2)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$userName = 'Анна'; // укажите здесь ваше имя
+$user_name = 'Анна'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -29,7 +29,7 @@ $userName = 'Анна'; // укажите здесь ваше имя
         <nav class="user-menu">
     <?php if ($is_auth === 1): ?>
     <div class="user-menu__logged">
-  <p><?= $userName; ?></p>
+  <p><?= $user_name; ?></p>
   <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
   <a class="user-menu__logout" href="#">Выход</a>
     </div>
